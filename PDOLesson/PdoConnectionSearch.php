69676e94f44bd27e11a7a0581bb7b949ed1cb985
Davi-o <alvesdelima.davi@gmail.com>
2020-01-28 22:48:42 -0300

    Search e Insert feitos

--- PDOLesson/PdoConnectionSearch.php
+++ PDOLesson/PdoConnectionSearch.php
@@ -7,9 +7,10 @@ $connection = new PDO("mysql:dbname=database_php7;host=localhost", "admin", "pas
  $statement->execute();
  $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
- foreach ($results as $column){
-     foreach ($column as $columnName => $value){
-         echo $columnName." => ". $value;
+ foreach ($results as $row){
+     foreach ($row as $column => $value){
+         echo $column." => ". $value;
          echo "<br>";
      }
+     echo "+++++++++++++++++++++++++++++++++++++++++++++<br>";
  }
